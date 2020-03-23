84c8e0d5678ad7330cf4e7945a9368a56cf640da
Tom <tsetterlund@infomine.com>
2020-03-23 09:10:51 -0700

    some basic setup

--- Handler.cs
+++ Handler.cs
@@ -9,7 +9,7 @@ namespace AwsDotnetCsharp
     {
        public Response Hello(Request request)
        {
-           return new Response("Go Serverless v1.0! Your function executed successfully!", request);
+           return new Response("Helllooooooo nuurse!", request);
        }
     }
 
