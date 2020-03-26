8df5af8923b18959b9a517bb0d3c3c1e8aa90774
Tom <tsetterlund@infomine.com>
2020-03-25 22:03:13 -0700

    Very basic working lambda with API Gateway

--- Handler.cs
+++ Handler.cs
@@ -1,39 +1,49 @@
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
-using System;
+using Newtonsoft.Json;
 
-[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace AwsDotnetCsharp
 {
     public class Handler
     {
-       public Response Hello(Request request)
-       {
-           return new Response("Helllooooooo nuurse!", request);
-       }
+        public APIGatewayProxyResponse Hello(APIGatewayProxyRequest request)
+        {
+            var body = JsonConvert.DeserializeObject<Request>(request.Body);
+            var Response = new Response("Helllooooooo nuurse!", body);
+            return new APIGatewayProxyResponse { StatusCode = 200, Body = JsonConvert.SerializeObject(Response) };
+        }
     }
 
     public class Response
     {
-      public string Message {get; set;}
-      public Request Request {get; set;}
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        [JsonProperty("request")]
+        public Request Request { get; set; }
 
-      public Response(string message, Request request){
-        Message = message;
-        Request = request;
-      }
+        public Response(string message, Request request)
+        {
+            Message = message;
+            Request = request;
+        }
     }
 
     public class Request
     {
-      public string Key1 {get; set;}
-      public string Key2 {get; set;}
-      public string Key3 {get; set;}
+        [JsonProperty("key1")]
+        public string Key1 { get; set; }
+        [JsonProperty("key2")]
+        public string Key2 { get; set; }
+        [JsonProperty("key3")]
+        public string Key3 { get; set; }
 
-      public Request(string key1, string key2, string key3){
-        Key1 = key1;
-        Key2 = key2;
-        Key3 = key3;
-      }
+        public Request(string key1, string key2, string key3)
+        {
+            Key1 = key1;
+            Key2 = key2;
+            Key3 = key3;
+        }
     }
 }
