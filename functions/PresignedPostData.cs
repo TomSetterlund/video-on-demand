b4194ba9dae7a012d0c85a680e88795d551a7b86
Tom <tsetterlund@infomine.com>
2020-04-05 15:42:32 -0700

    Added PresignedPostData lambda

--- functions/PresignedPostData.cs
+++ functions/PresignedPostData.cs
@@ -5,37 +5,45 @@ using Amazon.S3;
 using Amazon.S3.Model;
 using Newtonsoft.Json;
 using System;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
-
 namespace Handlers
 {
-    public class Save
+    public class PresignedPostData
     {
-        private readonly string _bucketName = Environment.GetEnvironmentVariable("S3_PREPROCESSED_NAME");
+        private readonly string _bucketName;
         private readonly RegionEndpoint _bucketRegion;
         private readonly IAmazonS3 _s3Client;
-        public Save()
+        public PresignedPostData()
         {
+            if (String.IsNullOrEmpty(Environment.GetEnvironmentVariable("S3_PREPROCESSED_NAME")))
+            {
+                throw new Exception("Your Preprocessd bucket name doesn't exist as an environment variable!");
+            }
+            _bucketName = Environment.GetEnvironmentVariable("S3_PREPROCESSED_NAME");
             _bucketRegion = RegionEndpoint.USWest2;
             _s3Client = new AmazonS3Client(_bucketRegion);
         }
 
-        public async Task<APIGatewayProxyResponse> Run(APIGatewayProxyRequest request)
+        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request)
         {
             try
             {
                 var body = JsonConvert.DeserializeObject<Request>(request.Body);
-
-                var putRequest = new PutObjectRequest 
-                {
+                var presignedUrlRequest = new GetPreSignedUrlRequest {
                     BucketName = _bucketName,
                     Key = body.ChallengeName,
-                    ContentBody = body.EncodedImg
+                    Expires = DateTime.Now.AddMinutes(3)
                 };
-                await _s3Client.PutObjectAsync(putRequest);
-                return new APIGatewayProxyResponse { StatusCode = 200, Body = "Success!" };
+
+                var response = _s3Client.GetPreSignedURL(presignedUrlRequest);
+                var respHeaders = new Dictionary<string, string> {
+                    { "Access-Control-Allow-Origin", "*" },
+                    { "Access-Control-Allow-Credentials", "true" }
+                };
+
+                return new APIGatewayProxyResponse { StatusCode = 200, Body = response, Headers = respHeaders };
             }
             catch (AmazonS3Exception e)
             {
@@ -62,13 +70,11 @@ namespace Handlers
         public string UserId { get; set; }
         [JsonProperty("challenge_name")]
         public string ChallengeName { get; set; }
-        [JsonProperty("encoded_image")]
-        public string EncodedImg { get; set; }
 
-        public Request(string userId, string encodedImage)
+        public Request(string userId, string challengeName)
         {
             UserId = userId;
-            EncodedImg = encodedImage;
+            ChallengeName = challengeName;
         }
     }
 }
